220e6bdbfd8e009ffcc3d0fe7a0cae0132cb64f5
GenericJM <juan.arturo.m.m@gmail.com>
2023-06-06 08:01:51 -0400

    test mobile

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,3 +1,60 @@
+import foto from "../../assets/foto.png"
+import "./home.css"
 export default function Home(){
-    return(<h1>Hola mundo</h1>)
+ 
+  return( 
+    
+    <>
+
+      <Personalinfo/>
+      <Experience/>
+    </>
+  
+  ) 
+}
+const Personalinfo=()=>{
+  return(
+    <section className="Personalinfo section">
+      <picture className="image-container">
+        <img className="Personalinfo-image" src={foto}/>
+      </picture>
+     
+      <div className="Personalinfo-contact">
+        <div className="main-info">
+          <div>
+            <h1 className="Personalinfo-name title">Juan Marcano</h1>
+            <h3 className="text">aspirante</h3>
+          </div>
+        
+          <div className="text">
+            <div className="email">
+              <span className="material-symbols-outlined">
+          mail
+              </span>  
+              <h2>infoJuanMarcano@gmail.com</h2>
+            </div>
+            <div className="phone">
+              <span className="material-symbols-outlined">
+            call
+              </span>
+              <h2>(+58) 04248221022</h2> 
+
+            </div>
+          </div>
+        </div>
+        <p className="text">
+        Desarrollador motivado, que está dispuesto a aprender y 
+        crear aplicaciones de interfaz de usuario sobresalientes.
+        </p>
+      </div>
+     
+
+    
+    </section>
+  )
+}
+const Experience=()=>{
+  <section className="experience">
+
+  </section>
 }
